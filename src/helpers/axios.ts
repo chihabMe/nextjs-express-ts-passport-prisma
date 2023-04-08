70472abb49916d9ef2c90e43530e45924197f15a
chihabMg <chihab.mg.me@gmail.com>
2023-04-08 13:53:59 +0100

    increased axios timeout

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 //this instance will be used by next getServerSideProps to make requests to the server
 export const axiosServerInstance = axios.create({
   baseURL: process.env.HOST,
-  timeout: 1500,
+  timeout: 10000,
 
   withCredentials: true,
 
@@ -17,7 +17,8 @@ export const axiosServerInstance = axios.create({
 //this instance will be used by react on the frontend
 export const axiosClientInstance = axios.create({
   baseURL: "",
-  timeout: 1500,
+
+  timeout: 10000,
   withCredentials: true,
   headers: {
     "content-type": "applicatoin/json",
